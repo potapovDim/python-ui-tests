07f6c6b1aae5539280482f3f1982f438b2de24f8
potapovDim <potapovd3012@gmail.com>
2017-08-29 15:32:22 +0300

    headless

---
+++ headless-example/index.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--window-size=1920x1080")
+
+driver = webdriver.Chrome(chrome_options=chrome_options)
+driver.get("https://www.google.com")
+lucky_button = driver.find_element_by_css_selector("[name=btnI]")
+lucky_button.click()
+
+# capture the screen
+driver.get_screenshot_as_file("capture.png")
+driver.close()
